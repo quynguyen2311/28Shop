b67406b928bfb1a96153f19f59b9c8fa192cf9f4
Nguyen Mai Minh Quy <maiminhquyn@gmail.com>
2025-04-22 09:04:06 +0700

    update menu fecct and responsive, haven't updated fontsize of footer and highlight category yet

---
+++ main.js
@@ -0,0 +1,23 @@
+function deleteMenuHoverEffect(array){
+    array.forEach(element => {
+        element.addEventListener('mouseleave',(e) => {
+            e.target.classList.remove('menu_active');
+        })
+    });
+}
+
+function addMenuHoverEffect(array){
+    array.forEach(element => {
+        element.addEventListener('mouseenter',(e) =>{
+            e.target.classList.add('menu_active');
+        })
+    });
+    deleteMenuHoverEffect(array);
+}
+
+
+function main(){
+    const headerList = document.querySelectorAll(".header__list > li");
+    addMenuHoverEffect(headerList);
+}
+main();
